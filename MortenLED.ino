030d90c95f7569941077ae9efb623500a25a529c
Morten Kleveland <mortengkleveland@gmail.com>
2015-06-17 21:26:49 +0200

    Add fading between colors

--- MortenLED.ino
+++ MortenLED.ino
@@ -2,34 +2,57 @@
 
 #define NUM_LEDS 50
 #define COLOR_ORDER RGB
-#define BRIGHTNESS 200
+#define BRIGHTNESS 100
 #define FRAMES_PER_SECOND 60
 #define DATA_PIN 6
 #define COOLING 55
 #define SPARKING 120
 
 const int SENSOR_PIN = 2;
+const int ANALOG_IN = A0;
 int sensorState = 0;
 int prevSensorState = sensorState;
 CRGB leds[NUM_LEDS];
 
 void setup() { 
+  Serial.begin(9600);
   delay(1000);
   pinMode(SENSOR_PIN, INPUT);
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, DATA_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
   random16_add_entropy(random());
   sensorState = digitalRead(SENSOR_PIN);
+  
   if (sensorState != prevSensorState) {
+    if(sensorState == HIGH) {
+      fade(0, 100, 0, 100, 0, 0, 500);
+      FastLED.show();
+      setColor(100, 0, 0);
+      FastLED.show();
+    }
     //blinkLED(100);
+    /*
     fire2012();
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] += CRGB(0, 20, 40);
+    }
+    */
+    
     FastLED.show();
   } else {
-    setColor(100, 0, 0);
+    if(sensorState == HIGH) {
+      Serial.println("halla");
+
+    }
+    //fire2012();
+
+    setColor(0, 100, 0);
+    FastLED.show();
   }
+  prevSensorState = sensorState;
   FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
 
@@ -55,7 +78,7 @@ void fire2012() {
   }
 
   // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for(int k= NUM_LEDS - 1; k >= 2; k--) {
+  for(int k = NUM_LEDS - 1; k >= 2; k--) {
     heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
   }
   
@@ -76,6 +99,32 @@ void setColor(int red, int green, int blue) {
     leds[i].setRGB(green, red, blue); // R & G do for some reason have to be inverted
   }
   FastLED.show();
+}
+
+void fade(int fromRed, int fromGreen, int fromBlue, int toRed, int toGreen, int toBlue, int fadeTimeMilliseconds) {
+  
+  float deltaRed = (float)(fromRed - toRed) / fadeTimeMilliseconds;
+  float deltaGreen = (float)(fromGreen - toGreen) / fadeTimeMilliseconds;
+  float deltaBlue = (float)(fromBlue - toBlue) / fadeTimeMilliseconds;
+
+  float absRed = (float)fromRed;
+  float absGreen = (float)fromGreen;
+  float absBlue = (float)fromBlue;
+  
+  int counter = 0;
+  
+  while(counter++ < fadeTimeMilliseconds) {
+    absRed -= deltaRed;
+    absGreen -= deltaGreen;
+    absBlue -= deltaBlue;
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i].red = absRed;
+      leds[i].green = absGreen;
+      leds[i].blue = absBlue;
+    }
+    FastLED.show();
+    delay(1);
+  }
 }
 
 
